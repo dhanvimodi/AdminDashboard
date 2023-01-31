c1521799b4c0feaed4a9cde7df0e5bbbd0c710b3
dhanvimodi <dhanvi.modi@gmail.com>
2023-01-31 12:27:38 +0530

    sidebar functionality added

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,5 @@
 import { createContext, useMemo, useState } from "react";
 import { createTheme } from "@mui/material/styles";
-import { color, typography } from "@mui/system";
 
 export const tokens = (mode) => ({
   ...(mode === "dark"
@@ -20,7 +19,7 @@ export const tokens = (mode) => ({
           100: "#d0d1d5",
           200: "#a1a4ab",
           300: "#727681",
-          400: "#434957",
+          400: "#1f2a40",
           500: "#141b2d",
           600: "#101624",
           700: "#0c101b",
@@ -160,30 +159,30 @@ export const themeSettings=(mode)=>{
             }),
         },
         typography:{
-            fontFamily : ["Source Sans Pro" , "sans serif"].join(","),
+            fontFamily : ["Source Sans Pro" , "sans-serif"].join(","),
             fontSize: 12,
             h1:{
-                fontFamily : ["Source Sans Pro" , "sans-serif"].join(""),
+                fontFamily : ["Source Sans Pro" , "sans-serif"].join(","),
                 fontSize: 40,
             },
             h2:{
-                fontFamily : ["Source Sans Pro" , "sans-serif"].join(""),
+                fontFamily : ["Source Sans Pro" , "sans-serif"].join(","),
                 fontSize: 32,
             },
             h3:{
-                fontFamily : ["Source Sans Pro" , "sans-serif"].join(""),
+                fontFamily : ["Source Sans Pro" , "sans-serif"].join(","),
                 fontSize: 24,
             },
             h4:{
-                fontFamily : ["Source Sans Pro" , "sans-serif"].join(""),
+                fontFamily : ["Source Sans Pro" , "sans-serif"].join(","),
                 fontSize: 20,
             },
             h5:{
-                fontFamily : ["Source Sans Pro" , "sans-serif"].join(""),
+                fontFamily : ["Source Sans Pro" , "sans-serif"].join(","),
                 fontSize: 16,
             },
             h6:{
-                fontFamily : ["Source Sans Pro" , "sans-serif"].join(""),
+                fontFamily : ["Source Sans Pro" , "sans-serif"].join(","),
                 fontSize: 14,
             },
         }
