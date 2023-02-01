a24ad6bd1484af3bac993c4f1fdffbdca18bd070
dhanvimodi <dhanvi.modi@gmail.com>
2023-02-01 17:02:31 +0530

    form page added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Contacts from "./scenes/contacts"
 import Invoices from "./scenes/invoices"
 // import Bar from "./scenes/bar"
 // import Line from "./scenes/line"
-// import Form from "./scenes/form"
+ import Form from "./scenes/form"
 // import Pie from "./scenes/pie"
 // import FAQ from "./scenes/faq"
 // import Geography from "./scenes/geography"
@@ -37,9 +37,9 @@ function App() {
                  <Route path="/team" element={<Team/>}/>
                  <Route path="/contacts" element={<Contacts/>}/>
                  <Route path="/invoices" element={<Invoices/>}/>
-              {/*  
+              
                 <Route path="/form" element={<Form/>}/>
-                <Route path="/bar" element={<Bar/>}/>
+              {/*    <Route path="/bar" element={<Bar/>}/>
                 <Route path="/line" element={<Line/>}/>
                 <Route path="/faq" element={<FAQ/>}/>
                 <Route path="/geography" element={<Geography/>}/>
